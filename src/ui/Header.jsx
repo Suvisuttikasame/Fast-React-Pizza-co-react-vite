b68c49b7bb14b41698081318b3cce269b0b92100
Nuttayos Suvisuttikasame <nuttayossuvisuttikasame@MacBook-Pro-khxngNuttayos.local>
2023-09-04 00:54:16 +0700

    - [init] tailwind

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,7 +3,7 @@ import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
-    <header>
+    <header className="bg-yellow-400">
       <Link to="/">FAST React Pizza Co.</Link>
       <SearchOrder />
       <p>John</p>
